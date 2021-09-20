d7266886dac1f9865b25cf65bfe8fb2878df8e70
Hara Red <rtc6fg4.fejg2@gmail.com>
2021-09-20 18:35:09 +0300

    Improve Joint draft

--- code/command/Joint.cs
+++ code/command/Joint.cs
@@ -3,8 +3,11 @@ using Godot;
 public class Joint : Command
 {
     private Vector3? target;
-    private (float axisA, float axisB, float axisC)? targetJoint;
+    private (float A, float B, float C)? targetJoint;
+    private (float A, float B, float C) start;
     private float velocity;
+    private Controllable controllable;
+    private float progress = 0;
 
     public Joint(Vector3 target, float velocity)
     {
@@ -20,6 +23,7 @@ public class Joint : Command
 
     public void Init(Controllable controllable)
     {
+        this.controllable = controllable;
     }
 
     public State Process(float delta)
