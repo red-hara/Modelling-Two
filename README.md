ca4b0a2a85b9441cc9c0a73fe5e2282725daf063
Hara Red <rtc6fg4.fejg2@gmail.com>
2021-09-20 17:25:02 +0300

    Add joint motion draft and readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Modelling-Two
+
+Another simple modelling project.
+
+Joint motion is not yet implemented.
---
+++ code/command/Joint.cs
@@ -0,0 +1,29 @@
+using Godot;
+
+public class Joint : Command
+{
+    private Vector3? target;
+    private (float axisA, float axisB, float axisC) targetJoint;
+    private float velocity;
+
+    public Joint(Vector3 target, float velocity)
+    {
+        this.target = target;
+        this.velocity = velocity;
+    }
+
+    public Joint((float axisA, float axisB, float axisC) targetJoint, float velocity)
+    {
+        this.targetJoint = targetJoint;
+        this.velocity = velocity;
+    }
+
+    public void Init(Controllable controllable)
+    {
+    }
+
+    public State Process(float delta)
+    {
+        return State.Error;
+    }
+}
